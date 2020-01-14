a8241011a91c38c516005770129c02088e4eec2d
Valera Miheev <valeriy.mikheyev@gmail.com>
2020-01-14 13:12:35 +0200

    start Proj

---
+++ src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style >
+  div{
+    background-color: red;
+    width: 100px;
+    height: 1500px;
+  }
+</style>
